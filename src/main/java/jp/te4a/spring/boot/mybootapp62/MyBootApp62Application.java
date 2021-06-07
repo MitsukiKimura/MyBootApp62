b457f55695e92c07b5e2b04a0ec82dbf55eeea94
木村 光希 <180012@jc-21.jp>
2021-06-07 12:55:09 +0900

    二度と違うプロジェクトをチェãブラïにブランチをキルナ

---
+++ src/main/java/jp/te4a/spring/boot/mybootapp62/MyBootApp62Application.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.mybootapp62;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp62Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBootApp62Application.class, args);
+    }
+
+}
---
+++ src/test/java/jp/te4a/spring/boot/mybootapp62/MyBootApp62ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.mybootapp62;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootApp62ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
